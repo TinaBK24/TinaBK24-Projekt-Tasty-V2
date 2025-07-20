4ab81b6da6b41e582b2d71966b3639e59662439e
Khrystyna Barshchevska-Kostrova <barshchevskak@gmail.com>
2025-07-20 21:03:58 +0200

    Fehler behoben

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,15 +2,15 @@ const Footer = () => {
     return (
         <footer className="flex justify-center items-center gap-6 mt-40">
             <a href="https://www.instagram.com/buzzfeedtasty/#">
-                <img src="./img/Inst.png" alt="Instagram" />
+                <img src="/img/Inst.png" alt="Instagram" />
             </a>
 
             <a href="https://www.youtube.com/buzzfeedtasty">
-                <img src="./img/YouTube.png" alt="Youtube" />
+                <img src="/img/YouTube.png" alt="Youtube" />
             </a>
 
             <a href="https://www.facebook.com/buzzfeedtasty/">
-                <img src="./img/Facebook.png" alt="Facebook" />
+                <img src="/img/Facebook.png" alt="Facebook" />
             </a>
         </footer>
     );
